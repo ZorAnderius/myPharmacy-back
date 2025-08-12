b3058334672a23b262ea2faccd00bee6eb9e5691
ZorAnderius <gabriel.zorander@gmail.com>
2025-08-12 16:39:07 +0100

    add: notification if server configuration will be broken down

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bootstrap = () => {
     try {
         setupServer();
     } catch (error) {
-        
+        console.error('Error during server setup:', error);
+        process.exit(1);
     }
 }
 
