ea99159b5fe09ecd27cded231e4b3bfa9e3f8c4e
ZorAnderius <gabriel.zorander@gmail.com>
2025-08-12 16:36:59 +0100

    add: helmet for http headers and update  package dependencies

--- src/expressConfig/initApp.js
+++ src/expressConfig/initApp.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import morgan from './morganConfig.js';
 import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(morgan('tiny-colored'));
 app.use(cors());
+app.use(helmet());
 app.use(
     express.json({
-        type: ['application/json', 'application/vnd.api+json'],
+        type: ['application/json'],
     })
 );
+app.use(express.urlencoded({ extended: true, limit: '10kb'}));
+app.use(express.static('public'));
 
 export default app;
