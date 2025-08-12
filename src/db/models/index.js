a8bfba1c1d849dc890622ba7319179604ff3814b
ZorAnderius <gabriel.zorander@gmail.com>
2025-08-12 20:50:58 +0100

    add: index.js for centralized and scaleble model control

---
+++ src/db/models/index.js
@@ -0,0 +1,28 @@
+import { readdirSync } from 'node:fs';
+import { join, resolve } from 'node:path';
+import sequelize from '../sequelize.js';
+
+const modelsPath = resolve(process.cwd(), 'src', 'db', 'models');
+
+const db = {};
+
+const modelFiles = readdirSync(modelsPath).filter(file => file.endsWith('.js') && file !== 'index.js');
+
+for (const file of modelFiles) {
+  const module = await import(`file://${join(modelsPath, file)}`);
+  const model = module.default;
+  model.initModel(sequelize);
+  db[model.name] = model;
+}
+
+Object.values(db).forEach(model => {
+  if (typeof model.associate === 'function') {
+    model.associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+
+db.Sequelize = sequelize.Sequelize;
+
+export default db;
