b04a26383368406b6be3ece2be1ce77cdbeb2a3f
ZorAnderius <gabriel.zorander@gmail.com>
2025-08-13 19:35:58 +0100

    add: refreshToken sequelize model

---
+++ src/db/models/RefreshToken.js
@@ -0,0 +1,41 @@
+import { Model, DataTypes } from 'sequelize';
+
+class RefreshToken extends Model {
+  /**
+   * Initialize the model
+   * @param {import('sequelize').Sequelize} sequelize
+   * @return {typeof RefreshToken} The initialized RefreshToken model.
+   */
+  static initModel(sequelize) {
+    RefreshToken.init(
+      {
+        id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+        user_id: { type: DataTypes.UUID, allowNull: false },
+        jti: { type: DataTypes.STRING, allowNull: false, unique: true },
+        revoked: { type: DataTypes.BOOLEAN, defaultValue: false },
+        replaced_by: { type: DataTypes.UUID, allowNull: true },
+        expires_at: { type: DataTypes.DATE, allowNull: false },
+        ip: { type: DataTypes.STRING, allowNull: true },
+        user_agent: { type: DataTypes.STRING, allowNull: true },
+      },
+      {
+        sequelize,
+        modelName: 'RefreshToken',
+        tableName: 'refresh_tokens',
+        timestamps: true,
+        underscored: true,
+      }
+    );
+  }
+
+  /**
+   * Define associations
+   *
+   * @param {Object} models - All Sequelize models.
+   */
+  static associate(models) {
+    RefreshToken.belongsTo(models.User, { foreignKey: 'user_id' });
+  }
+}
+
+export default RefreshToken;
