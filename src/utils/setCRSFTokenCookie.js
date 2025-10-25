839462d5e483ce19de29dafad08a125f41e5dbd1
ZorAnderius <gabriel.zorander@gmail.com>
2025-10-25 22:42:35 +0100

    update: csrf issue

--- src/utils/setCRSFTokenCookie.js
+++ src/utils/setCRSFTokenCookie.js
@@ -6,8 +6,10 @@ export const setCSRFTokenCookie = (res, token) => {
   const cookieOptions = {
     httpOnly: false,
     secure: true, // Always secure for cross-origin requests
-    sameSite: 'None', // Allow cross-origin cookies
+    sameSite: 'None', // Required for cross-origin cookies
     maxAge: MAX_AGE_CSRF_TOKEN * 1000,
+    path: '/',
+    domain: undefined, // Let browser handle domain automatically
   };
   res.cookie(CSRF_TOKEN_COOKIE, token, cookieOptions);
 };
@@ -15,7 +17,8 @@ export const setCSRFTokenCookie = (res, token) => {
 export const clearCSRFTokenCookie = res => {
   res.clearCookie(CSRF_TOKEN_COOKIE, {
     httpOnly: false,
-    secure: true, // Always secure for cross-origin requests
-    sameSite: 'None', // Allow cross-origin cookies
+    secure: true,
+    sameSite: 'None',
+    path: '/',
   });
 };
